1d3f7db7b429c610455727dfc0504379cd1a5ff2
HappyCrazyUnicorn <pankova.mary.v@gmail.com>
2022-04-10 17:25:37 +0300

    Additional test

--- src/test/java/ru/netology/CI/statistics/StatServiceTest.java
+++ src/test/java/ru/netology/CI/statistics/StatServiceTest.java
@@ -17,4 +17,44 @@ public class StatServiceTest {
 
         assertEquals(expected, actual);
     }
+    void findMaxFirst() {
+        StatService service = new StatService();
+
+        long[] incomesInBillions2 = {3,2,1};
+        long expected = 3;
+
+        long actual = service.findMax(incomesInBillions2);
+
+        assertEquals(expected, actual);
+    }
+    void findMaxLast() {
+        StatService service = new StatService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 122};
+        long expected = 122;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    void findMaxEquals() {
+        StatService service = new StatService();
+
+        long[] incomesInBillions2 = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions2);
+
+        assertEquals(expected, actual);
+    }
+    void findMaxMiddle() {
+        StatService service = new StatService();
+
+        long[] incomesInBillions2 = {12, 5, 8, 4, 5, 3, 18, 6, 11, 11, 12};
+        long expected = 18;
+
+        long actual = service.findMax(incomesInBillions2);
+
+        assertEquals(expected, actual);
+    }
 }
